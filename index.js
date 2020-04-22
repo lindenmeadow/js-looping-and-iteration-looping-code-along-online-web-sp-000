0a1783f42d6a19a2895a931d838bf5a1e23b811e
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-22 13:41:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+function writeCards(names, ev) {
+  let messages = [];
+  for(let i = 0; i < names.length; i++) {
+    let m = `Thank you, ${names[i]}, for the wonderful ${ev} gift!`;
+    messages.push(m);
+  }
+  return messages;
+}
