141124de8d2e44cd3a5f88b987a95a19839664a2
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-22 13:47:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, ev) {
+  let messages = [];
+  for(let i = 0; i < names.length; i++) {
+    let m = `Thank you, ${names[i]}, for the wonderful ${ev} gift!`;
+    messages.push(m);
+  }
+  return messages;
+}
+
+function countDown(int) {
+  while (int >= 0) {
+    console.log(int--);
+  }
+}
